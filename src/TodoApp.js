58b25fbc6299b6ceca992252e97f1e5424bf4c5d
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-03-23 17:55:36 +0100

    Add state and lifecycle methods

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,16 +6,50 @@ import { Footer } from './Footer'
 import { TodoFooter } from './TodoFooter'
 import { TodoToggleAll } from './TodoToggleAll'
 
-export function TodoApp(props) {
-  return <div>
-    <section className="todoapp">
-			<TodoForm />
-			<section className="main">
-        <TodoToggleAll />
-				<TodoList todos={props.todoItems}/>
+export class TodoApp extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			todos: [
+				{
+					title: 'Learn state',
+					isDone: false,
+					id: 0
+				}
+			]
+		}
+	}
+
+	componentDidMount () {
+		this.interval = setInterval(() => {
+			console.log('hello!')
+			const newTodo = {
+				title: 'nowy task',
+				isDone: false,
+				id: 1
+			}
+			const newState = {
+				todos: [...this.state.todos, newTodo]
+			}
+			this.setState(newState)
+		}, 2000)	
+	}
+
+	componentWillUnmount () {
+		clearInterval(this.interval)
+	}
+
+  render() {
+		return <div>
+			<section className="todoapp">
+				<TodoForm />
+				<section className="main">
+					<TodoToggleAll />
+					<TodoList todos={this.state.todos}/>
+				</section>
+				<TodoFooter todos={this.state.todos}/>
 			</section>
-			<TodoFooter todos={props.todoItems}/>
-		</section>
-		<Footer />
-  </div>
+			<Footer />
+		</div>
+	}
 }
